9b51416a98c3e90e6245b17bc76dd6de13ca00c0
sangwan2 <yesdouble@navre.com>
2020-06-17 20:40:51 +0900

    #3.4 life cycle component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import React from "react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  componentWillUnmount() {
+    console.log("goodbye component");
+  }
+
+  render() {
+    console.log("I'm a render");
+
+    return (
+      <div>
+        <h1>The Number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
