e16241d55eeaa2e2975c6a273288139a52c58752
sangwan2 <yesdouble@navre.com>
2020-06-27 22:53:53 +0900

    Updates

---
+++ precache-manifest.6d1f5947064e6d9809ea4bd37b0649ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650428ca61e4ea7fc6183a1f9bc51b6c",
+    "url": "/sangwan2/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4598eacbb8c0f2be35c7",
+    "url": "/sangwan2/movie_app_2020/static/css/main.93517c7d.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/sangwan2/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sangwan2/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4598eacbb8c0f2be35c7",
+    "url": "/sangwan2/movie_app_2020/static/js/main.52c735c7.chunk.js"
+  },
+  {
+    "revision": "d4f6082e85263e408869",
+    "url": "/sangwan2/movie_app_2020/static/js/runtime-main.ddde5ada.js"
+  }
+]);
