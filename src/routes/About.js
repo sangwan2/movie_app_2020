f27625da71ec7e9b447dcd5397ce32994c2d0f40
sangwan2 <yesdouble@navre.com>
2020-06-28 17:53:09 +0900

    #6.1

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <span>
+      About this page: I built it because I love movies. 한글도 써보자
+    </span>
+  );
+}
+
+export default About;
